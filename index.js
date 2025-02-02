ed959db118b49c64eacd1edd53ed43eaa8c7f4ea
Rajendra Chimala <rajendrachimala5@gmail.com>
2025-02-02 14:49:42 +0545

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use("/api",require('./Router/userRouter.js'));
 app.get('/',(req,res)=>{
-    res.end(`<div style="display:flex; height:100vh; align-items:center; justify-content:center; background:gray;"><h1 style="color:white;text-align:center; background:black; padding:20px; border-radius:10px">This is Day 1 Project of #7Day #7ProjectsChallenge</h1></div>`)
+    res.end(`<div style="display:flex; height:100vh; align-items:center; justify-content:center; background:gray;"><h1 style="color:white;text-align:center;font-size:40px; background:black; padding:20px; border-radius:10px">This is Day 1 Project of #7Day #7ProjectsChallenge</h1></div>`)
 })
 app.use("/api/contact/",require('./Router/contactRoute'));
 
